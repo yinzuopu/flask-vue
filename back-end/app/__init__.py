48b57cbd4a7c8879e013f2f9188be16db4ed2907
hulkyin <hulkyin@tencent.com>
2023-11-28 17:28:51 +0800

    2. Vue.js通过axios访问Flask RESTful API

---
+++ back-end/app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+from config import Config
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # enable CORS
+    CORS(app)
+
+    # 注册蓝图
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    return app
---
+++ back-end/app/api/__init__.py
@@ -0,0 +1,5 @@
+from flask import Blueprint
+bp = Blueprint('api', __name__)
+
+# 写在最后防止循环导入，ping.py文件也会导入bp
+from app.api import ping
